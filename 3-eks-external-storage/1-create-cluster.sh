9b36617767bbd540988607a10394bafe25082654
gary-archer <garyarcher36@gmail.com>
2024-11-03 11:58:21 +0000

    Updates

--- 3-eks-external-storage/1-create-cluster.sh
+++ 3-eks-external-storage/1-create-cluster.sh
@@ -12,33 +12,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-{
- “Version”: “2012-10-17”,
- “Statement”: [
- {
- “Effect”: “Allow”,
- “Action”: [
- “ec2:AttachVolume”,
- “ec2:CreateSnapshot”,
- “ec2:CreateTags”,
- “ec2:CreateVolume”,
- “ec2:DeleteSnapshot”,
- “ec2:DeleteTags”,
- “ec2:DeleteVolume”,
- “ec2:DescribeAvailabilityZones”,
- “ec2:DescribeInstances”,
- “ec2:DescribeSnapshots”,
- “ec2:DescribeTags”,
- “ec2:DescribeVolumes”,
- “ec2:DescribeVolumesModifications”,
- “ec2:DetachVolume”,
- “ec2:ModifyVolume”
- ],
- “Resource”: “*”
- }
- ]
-}
-
 #
 # Install the EBS CSI driver
 #
